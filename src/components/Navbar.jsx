f0517fe1f25b0ccc7a77be8ab1d0a4433e1aaa8c
Praveenkumar1434 <mpraveen1432004@gmail.com>
2025-08-10 16:57:41 +0530

    modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { ChevronDownIcon } from '@heroicons/react/24/solid';
+import { FaChevronDown } from 'react-icons/fa';
 import indi from '../assets/images/Indigo.avif';
 
 const Navbar = () => {
@@ -15,15 +15,15 @@ const Navbar = () => {
                     {/* Center Menu */}
                     <div className="hidden md:flex items-center gap-10  text-sm bg-[#F0F9FF]  font-medium text-gray-800">
                         <a href="#" className="flex items-center gap-1">
-                            Book <ChevronDownIcon className="h-4 w-4 text-gray-600  " />
+                            Book <FaChevronDown className="h-4 w-4 text-gray-600  " />
                         </a>
                         <a href="#" className="flex items-center gap-1  ">
-                            Trips <ChevronDownIcon className="h-4 w-4 text-gray-600  " />
+                            Trips <FaChevronDown className="h-4 w-4 text-gray-600  " />
                         </a>
                         <a href="#" className=' '>Deals and Offers</a>
                         <a href="#" className=' '>Check-in</a>
                         <a href="#" className="flex items-center gap-1 ">
-                            IndiGo BluChip <ChevronDownIcon className="h-4 w-4 text-gray-600 " />
+                            IndiGo BluChip <FaChevronDown className="h-4 w-4 text-gray-600 " />
                         </a>
                     </div>
 
@@ -33,7 +33,7 @@ const Navbar = () => {
 
                         {/* Login Button */}
                         <button className="flex items-center gap-1 bg-gray-300 rounded-full px-4 py-2 text-sm ">
-                            Login <ChevronDownIcon className="h-4 w-4 text-gray-600 " />
+                            Login <FaChevronDown className="h-4 w-4 text-gray-600 " />
                         </button>
                     </div>
                 </div>
